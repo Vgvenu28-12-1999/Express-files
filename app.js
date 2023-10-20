d0d34a87c2dc6a6cad667eeb8d533515ce8e9c81
Vgvenu28-12-1999 <89677083+Vgvenu28-12-1999@users.noreply.github.com>
2023-10-20 20:48:00 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,60 +1,30 @@
- 
+const path = require('path') 
 // fs allows us to work with the file system
 const express= require('express')
-const fs = require('fs')
 const bodyparser  = require('body-parser')
 const app = express()
-app.use(bodyparser.urlencoded());
-app.get('/',(req,res,next) =>{
-     fs.readFile('username.txt',(err,data)=>{
-        if(err){
-            console.log(err)
-            data = 'No chat exist'
-        }
-        res.send(`${data}<form action= "/" method="POST" onSubmit = "document.getElementById('username').value=localStorage.getItem(username)"> 
-    <input type ="text" name="message" id="message">
-    <input type = "hidden" name="username" id="username>"
-    <br />
-    <button type ="submit">Send</button>
 
-    </form>`
-    )
-     })
-    
-        
-    });
-    app.post("/",(req,res,next)=>{
-        console.log(req.body.username)
-        console.log(req.body.message)
-        fs.writeFile("username.txt",`${req.body.username}: ${req.body.message}`,{flag:'a'}, (err)=>
-            err ? console.log(err) : res.redirect("/")
-        )
-    })
-
-    app.get("/login",(req,res,next) =>{
-
-        res.send(`<form action= "/login" method="POST" onSubmit = "document.getElementById('username').value=localStorage.getItem(username)"> 
-        
-        <input type ="text" name="message" id="message">
-        
-        <br />
-        <button type ="submit">Login</button>
-    
-        </form>`
-        )
-    });            
-
-    app.post("/login",(req,res,next)=>{
-        console.log(req.body.username)
-        console.log(req.body.message)
-        fs.writeFile("username.txt",` ${req.body.message}`, (err)=>
-            err ? console.log(err) : res.redirect("/")
-        )
-    })
+const adminroutes = require('./routes/admin')
+const shoproutes = require('./routes/shop')
+const successRoutes = require('./routes/success');
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname,'public')))
 
 
-app.listen(5303)
-// learn start fron express 9 video
+app.use('/admin',adminroutes)
+
+app.use(shoproutes)
+app.use('/success',successRoutes);
+
+app.use('/contact',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'view','contact.html'));
+})
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'view','404.html'));
+})
+
+app.listen(4303)
+ // learn start fron express 9 video
 
 
     
